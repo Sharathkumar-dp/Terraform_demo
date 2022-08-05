0e14b7720b22d289e393f4f6ac8a3ee287bcc112
Sharathkumar <dpsharath.msr@gmail.com>
2022-08-05 07:57:35 +0000

    3rd update

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install httpd -y
 sudo service httpd start
 sudo service enable httpd
-echo " <html> <body> <h1>Hello World!...</h1> <p>This is terraform demo Instance</p> <p> Hostaname:- </p>  </body>  </html>  $(hostname -f)  " > /var/www/html/index.html
+echo " <html> <body> <h1>Hello World !...</h1> <p>This is terraform demo Instance</p> <p> Hostaname:- </p>  </body>  </html>  $(hostname -f)  " > /var/www/html/index.html
